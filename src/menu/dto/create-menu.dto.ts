f875a3d7cd72352d609ca92b089be312d3528552
Karol Homziuk <karolhomziuk@gmail.com>
2022-05-23 11:28:19 +0200

    add class-validator on dto

--- src/menu/dto/create-menu.dto.ts
+++ src/menu/dto/create-menu.dto.ts
@@ -1,8 +1,14 @@
+import { IsString, IsNumber } from 'class-validator';
 import { MealType } from 'src/shared/enums/meal-type.enum';
-
 export class CreateMenuDto {
+  @IsString()
   name: string;
-  mealType: MealType;
+
+  type: MealType;
+
+  @IsNumber()
   price: number;
+
+  @IsString()
   description: string;
 }
